5677893f4bd274ce4ee62681e9b398f311f579e6
r0302772 <dennis.nijs@hotmail.com>
2024-09-04 16:20:59 +0200

    Chapter 1 Sequence example

---
+++ 1 Sequence/Sequence example/Example 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // To change the background color of the screen (Console)
+            Console.BackgroundColor = ConsoleColor.White;
+            // To change the foreground color of the screen, the color of the text
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            // Clears the console so the colors take effect immediately
+            Console.Clear();
+            // To change the content of the title bar of the screen.
+            Console.Title = "Example 1";
+
+
+            // Display a message AND goes to the next line
+            Console.WriteLine("Hello, future C# developer!");
+            // Adds a blank line for better spacing
+            Console.WriteLine();
+
+            // Display a message
+            Console.Write("Press enter to continue!");
+            // Wait for the user to press Enter to continue
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1 Sequence/Sequence example/Example 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // To change the background color of the screen (Console)
+            Console.BackgroundColor = ConsoleColor.White;
+            // To change the foreground color of the screen, the color of the text
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            // Clears the console so the colors take effect immediately
+            Console.Clear();
+            // To change the content of the title bar of the screen.
+            Console.Title = "Example 2";
+
+            // Declare variables
+            string name = "Doe";
+            string surname = "John";
+
+            // Display a message
+            Console.Write("My name is " + name + ", ");
+            // Display a message AND goes to the next line
+            Console.WriteLine("my surname is " + surname + "!");
+            // Adds a blank line for better spacing
+            Console.WriteLine();
+            // Display a message AND goes to the next line
+            Console.WriteLine("My fullname is {0} {1}!", surname,name);
+
+            Console.Write("Press enter to continue!");
+            // Wait for the user to press Enter to continue
+            Console.ReadLine();
+        }
+    }
+}
