5c6b05e7aaaf2e4b3a1817a0f9fcebd5a1b6b16d
r0302772 <dennis.nijs@hotmail.com>
2024-09-06 16:43:52 +0200

    Chapter 3 Iteration For example

---
+++ 3 Iteration For/Iteration For example/Example 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set console colors
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.Title = "Example 1";
+
+            // Declare variables
+            int number, numberOfPrints;
+            string output;
+
+            // Read user input
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the desired number of prints: ");
+            numberOfPrints = int.Parse(Console.ReadLine());
+
+            output = "\n";
+
+            for (int i = 0; i < numberOfPrints; i++)
+            {
+                output += $"\t{number}\n";
+            }
+
+            // Display the result
+            Console.WriteLine(output);
+
+            // Wait for user input before closing
+            Console.WriteLine("\nPress enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
